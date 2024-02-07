65ad2d49fd77347a9bb91788b905cedde839744e
DeNt <mrfraosgmail.com>
2024-02-07 09:12:14 +0200

    TCHAR_HW(07.02)

---
+++ PROJECTS_HW/MB's_HW(06.02)/MB's_HW(06.02)/MB's.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h> 
+#include <tchar.h> 
+
+INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	char resume[100] = "Eto pizda naxui\0";
+	for (char* i = resume; i != "\0"; ++i) {
+
+		MessageBox( 0, TEXT("Реализация алгоритма программы непосредственно в функции WinMain"), TEXT("Окно сообщения"), MB_OK | MB_ICONINFORMATION);
+	}
+	return 0;
+}	
